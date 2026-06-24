72c1ecf1a80d1d5b223613dffc3c9dd3f8dfcc20
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2026-06-24 15:16:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("anio").textContent = new Date().getFullYear();
+
+document.getElementById("mensaje").addEventListener("click", function () {
+  alert("Gracias por visitar mi pagina personal.");
+});
